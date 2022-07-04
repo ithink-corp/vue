01b276c95a612b29bf71d75206210403a755f1f2
eludadev <111eluda111@gmail.com>
2022-07-04 22:53:48 +0100

    Add minimum height to thought item

--- src/components/ThoughtItem.vue
+++ src/components/ThoughtItem.vue
@@ -6,7 +6,7 @@ defineProps({
 
 <template>
   <p
-    class="rounded border-[1px] border-stone-50/10 bg-stone-800 px-2 py-1 text-sm text-stone-50"
+    class="min-h-[4rem] rounded border-[1px] border-stone-50/10 bg-stone-800 px-2 py-1 text-sm text-stone-50"
   >
     {{ message }}
   </p>
